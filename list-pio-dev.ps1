2c990099345fc8db16bed9f273d5bbb180e5367f
Christian Fobel <christian@fobel.net>
2019-01-30 11:24:34 -0500

    feat(list): show development link targets

--- list-pio-dev.ps1
+++ list-pio-dev.ps1
@@ -1,1 +1,14 @@
-foreach ($i in $(dir $env:CONDA_PREFIX\share\platformio\bin\.conda-dev)) { echo $i.Name }
+echo "# Linked PlatformIO development projects:"
+echo ""
+foreach ($i in $(dir $env:CONDA_PREFIX\share\platformio\bin\.conda-dev)) {
+    # Get target of development link.
+    $link_path = "$($i.FullName)\..\..\$($i.Name)"
+    if ((Test-Path $link_path) -and
+        ($(Get-Item -Path $link_path -Force).LinkType -eq "Junction")) {
+        $target = Get-Item -Path $link_path |
+            Select-Object -ExpandProperty Target;
+        echo "    $i -> $target";
+    } else {
+        echo "    $i";
+    }
+}
